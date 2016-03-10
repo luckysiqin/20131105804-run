37cf8d5f589dfda87ccb4aca9416250e7d655afa
luckysiqin <a20131105804@Mac28.local>
2016-03-10 10:39:31 +0800

    add
    arithmetic

---
+++ main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  20131105804-run
+//
+//  Created by 20131105804 on 16/3/10.
+//  Copyright © 2016年 20131105804. All rights reserved.
+//
+
+import Cocoa
+
+func add(a x:Int,b y:Int,c z:Int){
+  print("\(x)+\(y)+\(z)=\(x+y+z)")
+}
+add(a:10,b:20,c:30);
+
